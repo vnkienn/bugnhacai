e25204adfce2f304ae030cda1e792fc118366636
vnkienn <149071222+vnkienn@users.noreply.github.com>
2025-09-20 21:24:14 +0700

    Update modify_response.js

--- modify_response.js
+++ modify_response.js
@@ -5,7 +5,7 @@ let obj = JSON.parse($response.body);  // Chuyển response thành đối tượ
 if ($request.url.indexOf("user/money") !== -1) {
     // Nếu mã code là 0 thì chỉnh sửa giá trị "t" thành 1999.000
     if (obj.code === 0) {
-        obj.t = 199935625.000;  // Chỉnh sửa giá trị "t"
+        obj.t = 1999.000;  // Chỉnh sửa giá trị "t"
     }
 }
 // Kiểm tra nếu là API "timeline/game?gameId=161" (https://api.tk88.com/api/front/user/timeline/game?gameId=161)
@@ -20,6 +20,13 @@ else if ($request.url.indexOf("timeline") !== -1) {
     // Nếu mã code là 0 thì chỉnh sửa giá trị "userMoney" thành 199935625.000
     if (obj.code === 0) {
         obj.t.userMoney = 199935625.000;  // Chỉnh sửa giá trị "userMoney"
+    }
+}
+// Kiểm tra nếu là API "login" (https://api.wsmt8g.cc/v2/auth/token/login)
+else if ($request.url.indexOf("auth/token/login") !== -1) {
+    // Nếu mã code là 0 thì chỉnh sửa giá trị "wallet" thành 1000000
+    if (obj.data.wallet === "0") {
+        obj.data.wallet = "1000000";  // Chỉnh sửa giá trị "wallet"
     }
 }
 
