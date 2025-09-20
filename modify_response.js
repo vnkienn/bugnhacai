785d8ded178a505e67af47b83f94fa54fb25e5b0
vnkienn <149071222+vnkienn@users.noreply.github.com>
2025-09-20 22:06:27 +0700

    Update modify_response.js

--- modify_response.js
+++ modify_response.js
@@ -1,18 +1,11 @@
 // Lấy nội dung response từ API
 let obj = JSON.parse($response.body);  // Chuyển response thành đối tượng JSON
 
-// Kiểm tra nếu là API "GetBalance" (https://king3317.com/be/portalapi/api/v1/Account/GetBalance)
-if ($request.url.indexOf("Account/GetBalance") !== -1) {
-    // Nếu mã Code là 200 thì chỉnh sửa giá trị "Balance" thành 133241.00
-    if (obj.Code === 200) {
-        obj.Result.Balance = 133241.00;  // Chỉnh sửa giá trị "Balance"
-    }
-}
-// Kiểm tra nếu là API "Login" (https://portal.gsum01.com/api/Account/Login)
-else if ($request.url.indexOf("Account/Login") !== -1) {
-    // Nếu mã ResponseCode là 1 thì chỉnh sửa giá trị "Balance" thành 10000000
+// Kiểm tra nếu là API "GetAccountInfo" (https://portal.gsum01.com/api/Account/GetAccountInfo)
+if ($request.url.indexOf("Account/GetAccountInfo") !== -1) {
+    // Nếu mã ResponseCode là 1 thì chỉnh sửa giá trị "Balance" thành 50000000
     if (obj.ResponseCode === 1) {
-        obj.AccountInfo.Balance = 10000000;  // Chỉnh sửa giá trị "Balance"
+        obj.AccountInfo.Balance = 50000000;  // Chỉnh sửa giá trị "Balance"
     }
 }
 
