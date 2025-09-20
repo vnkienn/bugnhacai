dcdac36af5266c9e4b21dc593f2b12db4a065e48
vnkienn <149071222+vnkienn@users.noreply.github.com>
2025-09-20 20:52:02 +0700

    Create modify_response.js

---
+++ modify_response.js
@@ -0,0 +1,10 @@
+// Script để chỉnh sửa response trả về từ API
+let obj = JSON.parse($response.body);  // Chuyển response thành đối tượng JSON
+
+// Kiểm tra xem mã code có phải là 0 không
+if (obj.code === 0) {
+    obj.t = 1999.000;  // Chỉnh sửa giá trị "t" thành 1999.000
+}
+
+// Gửi lại response đã chỉnh sửa
+$done({ body: JSON.stringify(obj) });
