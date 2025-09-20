ae3a76b47f9975eded939dc77e57a428004f10d0
vnkienn <149071222+vnkienn@users.noreply.github.com>
2025-09-20 20:57:12 +0700

    Update modify_response.js

--- modify_response.js
+++ modify_response.js
@@ -3,7 +3,7 @@ let obj = JSON.parse($response.body);  // Chuyển response thành đối tượ
 
 // Kiểm tra xem mã code có phải là 0 không
 if (obj.code === 0) {
-    obj.t = 1999.000;  // Chỉnh sửa giá trị "t" thành 1999.000
+    obj.t = 199935625.000;  // Chỉnh sửa giá trị "t" thành 1999.000
 }
 
 // Gửi lại response đã chỉnh sửa
