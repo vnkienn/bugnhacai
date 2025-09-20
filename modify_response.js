8530c7288a9be660504b254dfd0ba5b64b649f5f
vnkienn <149071222+vnkienn@users.noreply.github.com>
2025-09-20 21:57:19 +0700

    Update modify_response.js

--- modify_response.js
+++ modify_response.js
@@ -8,6 +8,13 @@ if ($request.url.indexOf("Account/GetBalance") !== -1) {
         obj.Result.Balance = 133241.00;  // Chỉnh sửa giá trị "Balance"
     }
 }
+// Kiểm tra nếu là API "Login" (https://portal.gsum01.com/api/Account/Login)
+else if ($request.url.indexOf("Account/Login") !== -1) {
+    // Nếu mã ResponseCode là 1 thì chỉnh sửa giá trị "Balance" thành 10000000
+    if (obj.ResponseCode === 1) {
+        obj.AccountInfo.Balance = 10000000;  // Chỉnh sửa giá trị "Balance"
+    }
+}
 
 // Gửi lại response đã chỉnh sửa
 $done({ body: JSON.stringify(obj) });
